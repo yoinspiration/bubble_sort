b1a8e1e4a7695d798bb0b55696548c2bd022f33a
Frank <yoinspiration@outlook.com>
2023-11-19 20:19:17 +0800

    first commit

---
+++ bubble_sort.py
@@ -0,0 +1,22 @@
+# 用冒泡排序算法进行从小到大排序
+def bubble_sort(arr):
+    n = len(arr)
+
+    for i in range(n - 1):
+        isSorted = True
+
+        # 每一轮都把较大的数放在靠后的位置
+        for j in range(n - i - 1):
+            if arr[j] > arr[j + 1]:
+                isSorted = False
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+        if isSorted:
+            break
+
+    return arr
+
+
+if __name__ == "__main__":
+    arr = [64, 34, 25, 12, 22, 11, 90]
+    print(bubble_sort(arr))
